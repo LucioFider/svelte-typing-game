7aebbdff21ae3397f4bf048bbbe600a0fb882fe5
LucioFider <fidelislucio404@gmail.com>
2023-01-03 23:21:39 +1000

    5th-commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { blur } from "svelte/transition";
   import { tweened } from "svelte/motion";
 
   type Game = "waiting for input" | "in progress" | "game over";
   type Word = string;
 
   let game: Game = "waiting for input";
-  let seconds = 10;
+  let seconds = 60;
   let typedLetter = "";
 
-  let words: Word[] =
-    "The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog".split(
-      " "
-    );
+  let words: Word[] = [];
 
   let wordIndex = 0;
   let letterIndex = 0;
   let correctLetters = 0;
+  let toggleReset = false;
 
   let wordsPerMinute = tweened(0, { delay: 300, duration: 1000 });
   let accuracy = tweened(0, { delay: 1300, duration: 1000 });
@@ -24,6 +24,22 @@
   let letterEl: HTMLSpanElement;
   let inputEl: HTMLInputElement;
   let caretEl: HTMLDivElement;
+
+  function resetGame() {
+    toggleReset = !toggleReset;
+
+    setGameState("waiting for input");
+    getWords(100);
+
+    seconds = 30;
+    typedLetter = "";
+    wordIndex = 0;
+    letterIndex = 0;
+    correctLetters = 0;
+
+    $wordsPerMinute = 0;
+    $accuracy = 0;
+  }
 
   function updateGameState() {
     setLetter();
@@ -144,6 +160,15 @@
     const interval = setInterval(gameTimer, 1000);
   }
 
+  async function getWords(limit: number) {
+    const response = await fetch(`/api/words?limit=${limit}`);
+    words = await response.json();
+  }
+
+  function focusInput() {
+    inputEl.focus();
+  }
+
   function handleKeydown(event: KeyboardEvent) {
     if (event.code === "Space") {
       event.preventDefault();
@@ -156,6 +181,11 @@
       startGame();
     }
   }
+
+  onMount(() => {
+    getWords(200);
+    focusInput();
+  });
 </script>
 
 {#if game !== "game over"}
@@ -170,23 +200,44 @@
     />
 
     <div class="time">{seconds}</div>
+    {#key toggleReset}
+      <div in:blur|local bind:this={wordsEl} class="words">
+        {#each words as word}
+          <span class="word">
+            {#each word as letter}
+              <span class="letter">{letter}</span>
+            {/each}
+          </span>
+        {/each}
 
-    <div bind:this={wordsEl} class="words">
-      {#each words as word}
-        <span class="word">
-          {#each word as letter}
-            <span class="letter">{letter}</span>
-          {/each}
-        </span>
-      {/each}
+        <div bind:this={caretEl} class="caret" />
+      </div>
+    {/key}
 
-      <div bind:this={caretEl} class="caret" />
+    <div class="reset">
+      <button on:click={resetGame} aria-label="reset">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          fill="none"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3"
+          />
+        </svg>
+      </button>
     </div>
   </div>
 {/if}
 
 {#if game === "game over"}
-  <div class="results">
+  <div in:blur class="results">
     <div>
       <p class="title">wpm</p>
       <p class="score">{Math.trunc($wordsPerMinute)}</p>
@@ -196,12 +247,18 @@
       <p class="title">accuracy</p>
       <p class="score">{Math.trunc($accuracy)}%</p>
     </div>
+    <button on:click={resetGame} class="play">Play Again</button>
   </div>
 {/if}
 
 <style lang="scss">
   .game {
     position: relative;
+
+    .input {
+      position: absolute;
+      opacity: 0;
+    }
 
     .time {
       position: absolute;
@@ -214,6 +271,12 @@
 
     &[data-game="in progress"] .time {
       opacity: 1;
+    }
+    .reset {
+      width: 100%;
+      display: grid;
+      justify-content: center;
+      margin-top: 2rem;
     }
   }
 
